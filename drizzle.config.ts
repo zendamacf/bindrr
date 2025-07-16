282f64b584872769cb80a945df1ab1a474d79317
Zach Lang <zach.d.lang@gmail.com>
2025-07-16 20:33:53 +1200

    Drops SvelteKit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'drizzle-kit';
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
-  schema: './src/lib/server/db/schema.ts',
+  schemaFilter: ['public'],
+  schema: './src/lib/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: { url: process.env.DATABASE_URL },
   verbose: true,
--- src/routes/+page.server.ts
+++
@@ -1,5 +0,0 @@
-import type { Config } from '@sveltejs/kit';
-
-export const config: Config = {
-  runtime: 'nodejs22.x',
-};
